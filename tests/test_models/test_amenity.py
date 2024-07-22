881454dab60368f0d241a284ce15c6d440ac23b0
Andrew <49104171+carsonak@users.noreply.github.com>
2024-07-22 23:07:31 +0300

    Added tests for amenity and city

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """Module for test_amenity."""
 
+from datetime import datetime
 import unittest
+
+from models.amenity import Amenity
 
 
 class TestAmenity(unittest.TestCase):
     """Tests for Amenity."""
+
+    def test_new_instance_attributes(self) -> None:
+        """Test attributes of a new instance."""
+        u: Amenity = Amenity()
+
+        self.assertIsInstance(u.id, str)
+        self.assertIsInstance(u.created_at, datetime)
+        self.assertEqual(u.created_at, u.updated_at)
+        self.assertIsInstance(u.name, str)
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """Module for test_city."""
 
+from datetime import datetime
 import unittest
+
+from models.city import City
 
 
 class TestCity(unittest.TestCase):
     """Tests for City."""
+
+    def test_new_instance_attributes(self) -> None:
+        """Test attributes of a new instance."""
+        u: City = City()
+
+        self.assertIsInstance(u.id, str)
+        self.assertIsInstance(u.created_at, datetime)
+        self.assertEqual(u.created_at, u.updated_at)
+        self.assertIsInstance(u.state_id, str)
+        self.assertIsInstance(u.name, str)
